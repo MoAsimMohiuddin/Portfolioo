9e87612d72e7b127ae5df8f969642c8eaf9d4efe
Asim Mohiuddin <iamasimmohiuddin@gmail.com>
2023-11-26 16:45:29 +0530

    Add Github icon Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
+import githubIcon from '../assets/img/icons8-github.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
@@ -49,11 +48,11 @@ export const NavBar = () => {
               <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
-              <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
-              </div>
+            <div className="social-icon">
+                <a href="https://www.linkedin.com/in/asim-mohiuddin-10395b212/" target="_blank"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/MoAsimMohiuddin" target="_blank"><img src={githubIcon} alt="" /></a>
+                <a href="https://instagram.com/simplyasimm" target="_blank"><img src={navIcon3} alt="" /></a>
+            </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
               </HashLink>
