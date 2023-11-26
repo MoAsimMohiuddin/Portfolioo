471545f58f1b7802aa8775c00dcf3a719329893a
Asim Mohiuddin <iamasimmohiuddin@gmail.com>
2023-11-26 17:09:13 +0530

    Add formspree

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,49 +1,64 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg";
-import 'animate.css';
+import { useForm, ValidationError } from "@formspree/react";
+import "animate.css";
 
 export const Contact = () => {
   const formInitialDetails = {
-    message: ''
-  }
+    message: "",
+  };
   const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState('Send');
+  const [buttonText, setButtonText] = useState("Send");
 
   const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    console.log(formDetails);
+    setFormDetails({
+      ...formDetails,
+      [category]: value,
+    });
   };
+
+  const [state, handleSubmit] = useForm("xknlljrv");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
 
   return (
     <section className="contact" id="connect">
       <Container>
         <Row className="align-items-center">
-          <Col size={12} md={6}>  
-                <img src={contactImg}/>
+          <Col size={12} md={6}>
+            <img src={contactImg} />
           </Col>
           <Col size={12} md={6}>
-                <div>
-                <h2>Get In Touch</h2>
-                <form onSubmit={handleSubmit}>
-                  <Row>
-                    <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Send me a Message Anonymously🫣" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
-                      <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
-                  </Row>
-                </form>
-              </div>
+            <div>
+              <h2>Get In Touch</h2>
+              <form onSubmit={handleSubmit}>
+                <Row>
+                  <Col size={12} className="px-1">
+                    <textarea
+                      id="message"
+                      name="message"
+                      rows="6"
+                      value={formDetails.message}
+                      placeholder="Send me a Message Anonymously🫣"
+                      onChange={(e) => onFormUpdate("message", e.target.value)}
+                    ></textarea>
+                    <ValidationError
+                      prefix="Message"
+                      field="message"
+                      errors={state.errors}
+                    />
+                    <button type="submit" disabled={state.submitting}>
+                      <span>{buttonText}</span>
+                    </button>
+                  </Col>
+                </Row>
+              </form>
+            </div>
           </Col>
         </Row>
       </Container>
     </section>
-  )
-}
+  );
+};
