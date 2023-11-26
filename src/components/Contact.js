e61b62638ea161cf57dbcfc739332cac39449427
Asim Mohiuddin <iamasimmohiuddin@gmail.com>
2023-11-26 17:14:23 +0530

    Placeholder color Enchance

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -37,7 +37,6 @@ export const Contact = () => {
                 <Row>
                   <Col size={12} className="px-1">
                     <textarea
-                    style={{"color": "white"}}
                       id="message"
                       name="message"
                       rows="6"
